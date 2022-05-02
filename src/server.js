b609d502e8ec5eb12394f1a195b298aeb5233472
ohcoddee <ohcoddee@gmail.com>
2022-05-02 23:34:34 +0900

    Initialize Zoom Clone

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import res from "express/lib/response";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+// app.listen(3000);
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
